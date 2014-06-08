417e613bff052225ec7df11579df65941246f355
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-06-08 16:47:06 +0900

    create CentOS 6.5 x86_64 packer template

---
+++ scripts/mroonga.sh
@@ -0,0 +1,7 @@
+sudo yum install centos-release-SCL
+sudo rpm -ivh http://packages.groonga.org/centos/groonga-release-1.1.0-1.noarch.rpm
+sudo yum makecache
+sudo yum install -y mysql55-mysql-server mysql55-mysql
+sudo /etc/init.d/mysql55-mysqld start
+sudo chkconfig mysql55-mysqld on
+sudo yum install -y mysql55-mroonga
---
+++ scripts/tokenizer.sh
@@ -0,0 +1,2 @@
+sudo yum install -y groonga-tokenizer-mecab
+sudo yum install -y install groonga-normalizer-mysql
